d4f4b9c518d9c808f87279bbcf827bd973e7dc2f
robiulhasananim <mdrobiulhasananim@gmail.com>
2025-11-18 10:07:57 +0600

    feat: add third party app

---
+++ ThirdPartyAPP/app.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+URL = ""
+
+def get_data(id=None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    json_data = json.dumps(data)
+
+    r = requests.get(url=URL, data=json_data)
+
+    data = r.json()
+
+    print(data)
+
+get_data(1)   # call for id 1    
+
+
