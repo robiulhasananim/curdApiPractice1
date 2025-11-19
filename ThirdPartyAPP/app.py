0aca003656755256d23b4f360af63667bd05281f
robiulhasananim <mdrobiulhasananim@gmail.com>
2025-11-20 01:56:36 +0600

    feat: add delete

--- ThirdPartyAPP/app.py
+++ ThirdPartyAPP/app.py
@@ -58,6 +58,25 @@ def update_data():
     print(data)
 
 
-update_data()
+# update_data()
+
+
+def delete_data():
+    data = {
+        'id': 4
+    }
+
+    json_data = json.dumps(data)
+
+    r = requests.delete(url=URL, data=json_data)
+    # print(r.text)
+    # print(r.status_code)
+
+    data = r.json()
+
+    print(data)
+
+
+delete_data()
 
 
--- api/views.py
+++ api/views.py
@@ -53,3 +53,14 @@ def student_api(request):
 
         json_data = JSONRenderer().render(serializer.errors)
         return HttpResponse(json_data, content_type='application/json')
+    
+    if request.method =='DELETE':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id')
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        res = {'msg': 'Data Deleted !!'}
+        json_data = JSONRenderer().render(res)
+        return HttpResponse(json_data, content_type='application/json')
